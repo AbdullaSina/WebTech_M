140dfb7dc46085618f39eb4b9bf296ecf114c986
AbdullaSina <ibnasina98@gmail.com>
2026-03-24 12:12:43 +0600

    final fix

---
+++ Js_practice.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Js Code</title>
+</head>
+<body>
+    <center>
+        <h1> Hello this is the Js lecture</h1>
+</center>
+
+<script>
+
+    console.log("Hello world, I am from console");
+    alert("Welcome to the Js class");
+
+    var name="Ibna Sina";
+    var age= 25;
+    var is_human= true;
+    var x;
+    var y = null;
+    hello ="Here we did not give data";
+
+    console.log(typeof name);
+    console.log(typeof age);
+    console.log(typeof is_human);
+    console.log(typeof x);
+    console.log(typeof y);
+    console.log(typeof hello);
+
+    var num1 = 40;
+    var num2 = 60;
+     
+    console.log("The concate of number is :",+num1+num2);//concate
+    console.log("The sum of number is :",num1+num2);//addition
+    console.log("The subtraction of number is :",num1-num2);//subtraction
+    console.log("The multiplication of number is :",num1*num2);//multiplication
+    console.log("The division of number is :",num1/num2);//division
+    
+
+
+    </script>
+    </body>
+    </html>
